fd36a35811da1c9774bcbe0cf469f3802fa05813
Vinay0726 <vinaythaware143@gmail.com>
2024-11-08 12:07:21 +0530

    changes cofig file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  plugins: [react()],
-})
+  root: '.', // Points to the directory containing index.html
+  build: {
+    outDir: 'dist', // Output directory for production build
+  },
+   plugins: [react()],
+});
