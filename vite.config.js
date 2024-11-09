91dcd0bac0dabdead762f4134bd350a77fd6d6c4
Vinay0726 <vinaythaware143@gmail.com>
2024-11-09 11:17:03 +0530

    done deployment in netlify

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
-  root: '.', // Points to the directory containing index.html
-  build: {
-    outDir: 'dist', // Output directory for production build
-  },
-   plugins: [react()],
-});
+  plugins: [react()],
+})
+
+
